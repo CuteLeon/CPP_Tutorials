6d762e2fa7a832bba4ef33ab2a47ca5db5f4f601
Leon <Leon.Engineer@hotmail.com>
2022-11-24 16:06:27 +0800

    连续子数列的和：动态窗口；

---
+++ 连续子序列的和/连续子序列的和.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+typedef tuple<int, int> Result;
+typedef vector<Result> Results;
+typedef vector<int> Numbers;
+
+void Scan(Numbers& numbers, Results& results, int targetSum)
+{
+	Numbers leftSums;
+	leftSums.emplace_back(numbers[0]);
+	for (int index = 1; index < numbers.size(); index++)
+		leftSums.emplace_back(leftSums.back() + numbers.at(index));
+	int leftIndex = 0, rightIndex = 0;
+	while (rightIndex < numbers.size() &&
+		leftIndex < numbers.size())
+	{
+		int currentSum = leftSums[rightIndex] - leftSums[leftIndex] + numbers[leftIndex];
+		if (currentSum < targetSum)
+		{
+			rightIndex++;
+		}
+		else if (currentSum > targetSum)
+		{
+			leftIndex++;
+		}
+		else
+		{
+			results.emplace_back(make_tuple(leftIndex, rightIndex));
+			rightIndex++;
+			leftIndex++;
+		}
+	}
+
+}
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	Numbers numbers = { 1,4,2,5,7,3,5,7,9,5,3,5,7,8,3,1,3,4,5,7,8,9,6,5 };
+
+	Results results;
+	Scan(numbers, results, 20);
+	for (Result& result : results)
+	{
+		cout << get<0>(result) << " ~ " << get<1>(result) << endl;
+	}
+
+	system("pause");
+}
