007c7e76bcb1c7ccbd771baae57f3367cabda5df
Leon <Leon.Engineer@hotmail.com>
2022-11-24 16:30:06 +0800

    数组流的平均值；

---
+++ 数组流的平均值/数组流的平均值.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> Numbers;
+
+class Containers
+{
+public:
+	Containers(const int max)
+		: maxSize(max)
+	{
+	}
+	double Insert(int number)
+	{
+		if (currentSize < maxSize)
+		{
+			currentSize++;
+			numbers.emplace_back(number);
+			currentIndex++;
+		}
+		else
+		{
+			currentSum -= numbers[currentIndex];
+			numbers[currentIndex++] = number;
+		}
+		if (currentIndex == maxSize) currentIndex = 0;
+		currentSum += number;
+		return (double)currentSum / (double)currentSize;
+	}
+private:
+	int maxSize;
+	int currentSum = 0;
+	int currentSize = 0;
+	int currentIndex = 0;
+	Numbers numbers;
+};
+
+int main()
+{
+	cout << "Hello World!\n";
+	Containers container(5);
+	for (int i = 0; i < 20; i++)
+	{
+		cout << "Insert: " << i << ", returen: " << container.Insert(i) << endl;
+	}
+
+	system("pause");
+}
