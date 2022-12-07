467915447d643c9cbe1e06c6ec8d72e141fae2aa
Leon <Leon.Engineer@hotmail.com>
2022-12-07 21:51:51 +0800

    JiaQi problem 1 and 2;

---
+++ JiaQi_Problem_1/JiaQi_Problem_1.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <set>
+#include <string>
+
+using namespace std;
+
+/// <summary>
+/// Pattern matching in string
+/// PatternString only contains 3 kinds of different characters
+/// </summary>
+/// <param name="pattern">Pattern string</param>
+/// <param name="str">Source string</param>
+/// <returns>Count of matching result</returns>
+int patternMatch(string pattern, string str)
+{
+	map<char, int> patternIndex;
+	vector<int> patterns;
+	int lastIndex = 0;
+	for (int i = 0; i < pattern.length(); i++)
+	{
+		auto target = patternIndex.find(pattern[i]);
+		if (target == patternIndex.end())
+		{
+			patternIndex[pattern[i]] = lastIndex;
+			patterns.emplace_back(lastIndex);
+			lastIndex++;
+		}
+		else
+		{
+			patterns.emplace_back(target->second);
+		}
+	}
+
+	int result = 0;
+	for (int i = 0; i < str.length() - pattern.length(); i++)
+	{
+		set<char> savedChar;
+		map<int, char> matchChars;
+		bool matched = true;
+		for (int p = 0; p < pattern.size(); p++)
+		{
+			auto stringChar = str[i + p];
+			auto matchChar = matchChars.find(pattern[p]);
+			if (matchChar == matchChars.end())
+			{
+				if (savedChar.find(stringChar) != savedChar.end())
+				{
+					matched = false;
+					break;
+				}
+				savedChar.insert(stringChar);
+				matchChars[pattern[p]] = stringChar;
+			}
+			else
+			{
+				if (stringChar != matchChar->second)
+				{
+					matched = false;
+					break;
+				}
+			}
+		}
+		if (matched)
+		{
+			cout << "Pattern matched: " << str.substr(i, pattern.length()) << endl;
+			result++;
+		}
+	}
+
+	return result;
+}
+
+void printString(string str)
+{
+	int index = 0;
+	int length = 1;
+	int lineIndex = 0;
+	int width = sqrt(str.length());
+	while (index < str.length())
+	{
+		for (int j = lineIndex; j < width; j++)
+		{
+			cout << ' ';
+		}
+		cout << str.substr(index, length) << endl;
+		lineIndex++;
+		index += length;
+		length += 2;
+	}
+}
+
+int main()
+{
+	string str = "asdfasdfasdfafafasdsdfsadfasdfgfafasdffasdfasdfafasasfedfafafasdfasd";
+	set<char> counter;
+	for (int i = 0; i < str.size(); i++)
+	{
+		counter.insert(str[i]);
+	}
+	cout << counter.size() << endl;
+
+	patternMatch("abcbcb", str);
+	printString(str);
+	return 0;
+}
---
+++ JiaQi_Problem_2/JiaQi_Problem_2.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <set>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+struct Point
+{
+	Point(int x, int y)
+		: X(x)
+		, Y(y)
+	{
+	}
+
+	int X;
+	int Y;
+
+	bool operator<(const Point& p) const
+	{
+		if (this->X < p.X) return true;
+		if (this->X > p.X) return false;
+		if (this->Y < p.Y) return true;
+		return false;
+	}
+};
+typedef set<Point> Points;
+typedef tuple<Points, Point> Path;
+typedef vector<Path> Paths;
+typedef map<int, Paths> StepPaths;
+
+static StepPaths dp;
+
+/// <summary>
+/// 在二维网格中不可重复地行走，计算第N步有几种不同的路径
+/// </summary>
+/// <param name="N"></param>
+/// <returns></returns>
+long long solveProblem2(int N)
+{
+	Paths previousPaths;
+	auto previousStates = dp.find(N - 1);
+	if (previousStates == dp.end())
+	{
+		solveProblem2(N - 1);
+		previousStates = dp.find(N - 1);
+	}
+
+	previousPaths = previousStates->second;
+	for (auto& previousPath : previousPaths)
+	{
+		auto& previousPathPoints = get<0>(previousPath);
+		auto& previousPathTail = get<1>(previousPath);
+		const int x = previousPathTail.X;
+		const int y = previousPathTail.Y;
+
+		if (x > 0)
+		{
+			auto next = Point(x - 1, y);
+			if (previousPathPoints.count(next) == 0)
+			{
+				Points currentPoints(previousPathPoints);
+				currentPoints.insert(next);
+				Path currentPath(currentPoints, next);
+				dp[N].emplace_back(currentPath);
+			}
+		}
+
+		if (y > 0)
+		{
+			auto next = Point(x, y - 1);
+			if (previousPathPoints.count(next) == 0)
+			{
+				Points currentPoints(previousPathPoints);
+				currentPoints.insert(next);
+				Path currentPath(currentPoints, next);
+				dp[N].emplace_back(currentPath);
+			}
+		}
+
+		{
+			auto next = Point(x + 1, y);
+			if (previousPathPoints.count(next) == 0)
+			{
+				Points currentPoints(previousPathPoints);
+				currentPoints.insert(next);
+				Path currentPath(currentPoints, next);
+				dp[N].emplace_back(currentPath);
+			}
+		}
+
+		{
+			auto next = Point(x, y + 1);
+			if (previousPathPoints.count(next) == 0)
+			{
+				Points currentPoints(previousPathPoints);
+				currentPoints.insert(next);
+				Path currentPath(currentPoints, next);
+				dp[N].emplace_back(currentPath);
+			}
+		}
+	}
+	dp.erase(N - 1);
+	return dp[N].size();
+}
+
+int main()
+{
+	dp[0] = { Path(make_tuple(Points{ Point(0,0) }, Point(0, 0))) };
+	dp[1] = { Path(make_tuple(Points{ Point(0,0), Point(1, 0) }, Point(1, 0))) };
+	for (int i = 5; i <= 25; i++)
+	{
+		cout << solveProblem2(i) << endl;
+	}
+
+	getchar();
+	return 0;
+}
